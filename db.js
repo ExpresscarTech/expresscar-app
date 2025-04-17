6c2d6bba7f14b65ec029d5263ef6e82ab6431ada
Nuno <expresscar.mcn2@gmail.com>
2025-04-17 14:02:52 +0100

    Ajusta db.js para usar variáveis de ambiente em produção

--- db.js
+++ db.js
@@ -1,23 +1,18 @@
-// db.js
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'yamanote.proxy.rlwy.net',
-  port: 51303,
-  user: 'postgres',
-  password: 'WxRYOyrVRuqhTDIZSrvwjbbjRmYnfnuc',
-  database: 'railway'
-});
-
-// Testa a conexão (este código será executado quando db.js for importado)
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('Erro ao conectar no PostgreSQL:', err);
-  } else {
-    console.log('Conexão com PostgreSQL estabelecida com sucesso!');
-    release();
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  ssl: {
+    rejectUnauthorized: false, // importante para funcionar no Render/Railway
   }
 });
+
+pool.connect()
+  .then(() => console.log('Conexão com PostgreSQL estabelecida com sucesso!'))
+  .catch(err => console.error('Erro ao conectar no PostgreSQL:', err));
 
 module.exports = pool;
